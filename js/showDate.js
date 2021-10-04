c11da4accb5cd6a4d2a9acae35a1006ff9e2c8db
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-10-04 14:22:31 +0900

    ⚡️ 현재 날짜 보여주기

---
+++ js/showDate.js
@@ -0,0 +1,15 @@
+const today = document.querySelector('.today-date p');
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let now = new Date();
+    let y = now.getFullYear();
+    let m = now.getMonth() + 1;
+    if (m < 10){
+        m = '0' + m;
+    }
+    let d = now.getDate();
+    if (d < 10) {
+        d = '0' + d;
+    }
+    today.innerText = `${y}.${m}.${d}`;
+})
