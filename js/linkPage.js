d47ead07d4e9f614a7382e0d2bc0e9db76752429
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-10-16 22:04:07 +0900

    ⚡️ link page from main page to profile page

--- js/linkPage.js
+++ js/linkPage.js
@@ -1,5 +1,10 @@
 const myinfo = document.querySelector('.myinfobtn');
+const myProfile = document.querySelector('.myprofilebtn');
 
 myinfo.addEventListener('click', (()=>{
     location.href= "../pages/myPage.html";
+}))
+
+myProfile.addEventListener('click', (() => {
+    location.href = "../pages/myProfile.html";
 }))
