2ac3aa50820be3763e199a87d4d1a94101809687
liswkjts <liswktjs@gmail.com>
2021-12-22 13:05:51 +0900

    feat: ⚡️ localStorage관련 설정 앞으로 웹에서 사용할 localStorage에 관련해서 설정 store.js / todayStorageConstants.js / weekStorageConstants.js

---
+++ js/constants/todayStorageConstants.js
@@ -0,0 +1,7 @@
+export const TODAY_LOCALSTORAGE_NAME = 'today-plan';
+export const KEY_INDEX = 'index'
+export const KEY_DAY = 'day'
+export const KEY_PLANLIST = 'planlist'
+export const KEY_PLAN = 'plan'
+export const KEY_DONE = 'done'
+export const KEY_ACHIVEMENT = 'achivement'
---
+++ js/constants/weekStorageConstants.js
@@ -0,0 +1,6 @@
+export const WEEK_LOCALSTORAGE_NAME = 'week-plan';
+export const KEY_INDEX = 'index'
+export const KEY_PLANLIST = 'planlist'
+export const KEY_PLAN = 'plan'
+export const KEY_DONE = 'done'
+export const KEY_ACHIVEMENT = 'achivement'
